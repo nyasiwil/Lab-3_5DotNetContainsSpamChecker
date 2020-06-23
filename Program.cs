20d2820673a7663db9e9796c118d18f43e9be74a
Nyasia Williams <ny.williams123@gmail.com>
2020-06-23 12:03:47 -0400

    //Updated and works but needs refactoring!             // - More words in BlackList             // - Resolve Uppercase/Lowercase issue with words             // - Use external db or flatefile that is easy to update             //      without having to update the code.             // Added by th 0623200

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace Lab_3_5DotNetContainsSpamChecker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Updated and works but needs refactoring!
+            // - More words in BlackList
+            // - Resolve Uppercase/Lowercase issue with words
+            // - Use external db or flatefile that is easy to update
+            //      without having to update the code.
+            // Added by th 0623200
+            Console.WriteLine("Hello World Lab 3_5DotNetContainsSpamChecker!");
+
+            string blackListWord = "fudge";
+            bool isSpam = false;
+
+            string message = Console.ReadLine();
+
+            if (message.Contains(blackListWord))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam.");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is not spam.");
+            }
+
         }
     }
 }
